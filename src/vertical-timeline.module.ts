78b89525ab629b2f76293a601f7a1eaab9cb0294
Stefan Paul Noack <stefan@noack.software>
2018-03-21 15:20:54 +0100

    Add dependencies for mat-icon.

--- src/vertical-timeline.module.ts
+++ src/vertical-timeline.module.ts
@@ -1,12 +1,14 @@
 import { NgModule, ModuleWithProviders } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { MatIconModule } from '@angular/material';
 
 import { VerticalTimelineComponent } from './vertical-timeline.component';
 import {VerticalTimelineCardComponent} from './vertical-timeline-card.component';
 
 @NgModule({
   imports: [
-    CommonModule
+    CommonModule,
+    MatIconModule
   ],
   declarations: [
     VerticalTimelineComponent,
